e92263126e0706a51d2a7d563607264095e90978
Tygalee McHenry-Contractor <tygalee.mchenry@selectquote.com>
2023-07-21 07:42:21 -0500

    added alert

--- tyga-app/src/index.js
+++ tyga-app/src/index.js
@@ -4,21 +4,31 @@ import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = 'Tyga';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hey hey hey, React!</h1>,
+  <h1>Hey hey hey, {name}!</h1>,
   document.getElementById('root')
 );
 
-const name = 'Tyga';
-const intro = <p>Hi there, {name}</p>;
+var car = {
+  make: "Kia",
+  model: "Rio",
+  year: 2020,
+  owner: {name: "Tyga McHenry", state: "IL"},
+  action: function() {alert("Vroom!");}
+}
 
-root.render(
-  intro,
-  document.getElementById('intro')
-)
+//Get car owner's name and make:
+var carOwner = car.owner.name;
+
+var carMake = car.make;
+
+//Show message:
+alert("I like your " + carMake + "," + carOwner + "!")
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
