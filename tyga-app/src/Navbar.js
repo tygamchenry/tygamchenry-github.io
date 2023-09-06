da7c02cb9f85752d4c60af34656f2be6eb3a383a
Tygalee McHenry-Contractor <tygalee.mchenry@selectquote.com>
2023-09-05 23:55:35 -0500

    added nav

---
+++ tyga-app/src/Navbar.js
@@ -0,0 +1,19 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" class="site-title">Site Name</a>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/">About</a>
+            </li>
+            <li>
+                <a href="/">Projects</a>
+            </li>
+            <li>
+                <a href="/">Contact</a>
+            </li>
+        </ul>
+    </nav>
+}
--- tyga-app/src/index.js
+++ tyga-app/src/index.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
 
-const name = 'Tyga';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hey hey hey, {name}!</h1>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
 
-var car = {
-  make: "Kia",
-  model: "Rio",
-  year: 2020,
-  owner: {name: "Tyga McHenry", state: "IL"},
-  action: function() {alert("Vroom!");}
-}
-
-//Get car owner's name and make:
-var carOwner = car.owner.name;
-
-var carMake = car.make;
-
-//Show message:
-alert("I like your " + carMake + "," + carOwner + "!!")
 
 
 // If you want to start measuring performance in your app, pass a function
